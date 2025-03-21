d379eb3c0a1db245ed3ca4087c7d3438d626c2d8
RanimElhafy <60097726@udst.edu.qa>
2025-03-21 16:00:28 +0300

    Added DBUtils.java - Handles database connection and authentication

--- src/common/DBUtils.java
+++ src/common/DBUtils.java
@@ -1,24 +1,47 @@
 package common;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
-public class DBLogger {
-    public static void log(String level, String logger, String message, String username) {
-        String sql = "INSERT INTO logging (event_date, level, logger, message, username) VALUES (NOW(), ?, ?, ?, ?)";
-        try (Connection con = DBUtils.setUser("admin"); PreparedStatement stmt = con.prepareStatement(sql)) {
-            stmt.setString(1, level);
-            stmt.setString(2, logger);
-            stmt.setString(3, message);
-            if (username != null) {
-                stmt.setString(4, username);
-            } else {
-                stmt.setNull(4, java.sql.Types.VARCHAR);
-            }
-            stmt.executeUpdate();
-            DBUtils.closeConnection(con, null);
+public class DBUtils {
+    private static String url = "jdbc:mysql://127.0.0.1:3306/library_db";
+    private static String DBUsername = "libraryAdmin";
+    private static String DBPassword = "Lib@dmin123";
+
+    static Connection setUser(String role) {
+        String uname = "";
+        String pword = "";
+        if (role.equals("librarian")) {
+            uname = "libUser";
+            pword = "LibPass123!";
+        } else if (role.equals("admin")) {
+            uname = "adminUser";
+            pword = "AdminPass456!";
+        } else if (role.equals("member")) {
+            uname = "memberUser";
+            pword = "MemPass789!";
+        }
+        return establishConnection(uname, pword);
+    }
+
+    public static Connection establishConnection(String username, String password) {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, username, password);
+            System.out.println("Database Connection Successful");
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return con;
+    }
+
+    public static void closeConnection(Connection con, Statement stmt) {
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+            con.close();
+            System.out.println("Connection is closed");
+        } catch (SQLException e) {
+            e.getMessage();
         }
     }
 }
