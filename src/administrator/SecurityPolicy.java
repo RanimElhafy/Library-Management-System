b5ea538bf4fdb70e17ed09efe6a28790453894d9
aishasaeed08 <60104277@udst.edu.qa>
2025-03-22 15:41:10 +0300

    Create SecurityPolicy.java

---
+++ src/administrator/SecurityPolicy.java
@@ -0,0 +1,13 @@
+package administrator;
+
+public class SecurityPolicy {
+    private String description;
+
+    public SecurityPolicy(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
