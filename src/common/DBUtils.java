b35735d16bf3790fcb0973f736d9e2571e199607
aishasaeed08 <60104277@udst.edu.qa>
2025-03-24 15:25:31 +0300

    Update DBUtils.java

--- src/common/DBUtils.java
+++ src/common/DBUtils.java
@@ -2,25 +2,9 @@ package common;
 import java.sql.*;
 
 public class DBUtils {
-    private static String url = "jdbc:mysql://127.0.0.1:3306/library_db";
-    private static String DBUsername = "libraryAdmin";
-    private static String DBPassword = "Lib@dmin123";
-
-    static Connection setUser(String role) {
-        String uname = "";
-        String pword = "";
-        if (role.equals("librarian")) {
-            uname = "libUser";
-            pword = "LibPass123!";
-        } else if (role.equals("admin")) {
-            uname = "adminUser";
-            pword = "AdminPass456!";
-        } else if (role.equals("member")) {
-            uname = "memberUser";
-            pword = "MemPass789!";
-        }
-        return establishConnection(uname, pword);
-    }
+    private static String url = "jdbc:mysql://127.0.0.1:3306/librarymanagement";
+    private static String DBUsername = "root";
+    private static String DBPassword = "";
 
     public static Connection establishConnection(String username, String password) {
         Connection con = null;
@@ -28,9 +12,14 @@ public class DBUtils {
             con = DriverManager.getConnection(url, username, password);
             System.out.println("Database Connection Successful");
         } catch (SQLException e) {
+            e.printStackTrace();
             System.out.println(e.getMessage());
         }
         return con;
+    }
+
+    public static Connection establishConnection() {
+        return establishConnection("root", "");
     }
 
     public static void closeConnection(Connection con, Statement stmt) {
