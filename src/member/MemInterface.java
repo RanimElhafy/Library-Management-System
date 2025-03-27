935f6cf5eac792fb6cfff8532d2195cd24b21b3c
aishasaeed08 <60104277@udst.edu.qa>
2025-03-27 22:11:00 +0300

    Update MemInterface.java

--- src/member/MemInterface.java
+++ src/member/MemInterface.java
@@ -1,15 +1,32 @@
 package member;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 import java.sql.Connection;
 
 public class MemInterface {
-    public MemInterface(Stage stage, String username, Connection con) {
-        System.out.println("[MEMBER] Interface initialized for: " + username);
-        // TODO: Use stage and con if needed for further functionality
+    private Stage stage;
+    private String username;
+    private Connection connection;
+
+    public MemInterface(Stage stage, String username, Connection connection) {
+        this.stage = stage;
+        this.username = username;
+        this.connection = connection;
     }
 
     public void initializeComponents() {
-        System.out.println("Member components initialized.");
+        VBox layout = new VBox(20);
+        Label welcome = new Label("Welcome Member: " + username);
+
+        layout.getChildren().addAll(welcome);
+
+        Scene scene = new Scene(layout, 600, 400);
+        stage.setScene(scene);
+        stage.setTitle("Member Dashboard");
+        stage.show();
     }
 }
